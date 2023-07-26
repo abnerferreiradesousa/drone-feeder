92fd29edc1d50bc23b682fe2bed3d702898cee60
Abner <abnerferreira84@gmail.com>
2023-07-26 16:13:20 -0300

    start: Iniciando base do projeto com configuração da conexão com o banco de dados MySQL e criando os packages de controller, repository, entity, e por fim, já com a entidade Customer implementada.

---
+++ src/main/java/com/br/deliveryrobot/entity/Customer.java
@@ -0,0 +1,37 @@
+package com.br.deliveryrobot.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "t_customer")
+public class Customer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  private String email;
+  private String address;
+
+}
---
+++ src/test/java/com/br/deliveryrobot/DroneFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.deliveryrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
