bedb41520d5e1a3a4b8740dcd4b00753fce76072
Abner <abnerferreira84@gmail.com>
2023-08-16 10:29:48 -0300

    doc: DeliverydroneService's documentation added.

--- src/main/java/com/br/deliveryrobot/service/DeliverydroneService.java
+++ src/main/java/com/br/deliveryrobot/service/DeliverydroneService.java
@@ -12,10 +12,22 @@ public class DeliverydroneService {
   @Autowired
   private DeliverydroneRepository deliverydroneRepository;
 
+  /**
+   * Create a given drone.
+   * 
+   * @param drone Drone which will be registered.
+   * @return The drone that was persisted.
+   */
   public Deliverydrone registerDrone(Deliverydrone drone) {
     return this.deliverydroneRepository.save(drone);
   }
 
+  /**
+   * Find drone by id.
+   * 
+   * @param droneId Drone's id that will be searched.
+   * @return If drone exists will return the own drone, otherwise it will throw an exception.
+   */
   public Deliverydrone getDroneById(long droneId) {
     Deliverydrone droneSearched = this.deliverydroneRepository.findById(droneId)
         .orElseThrow(() -> new NotFoundException("Não encontrado!"));
@@ -23,6 +35,13 @@ public class DeliverydroneService {
     return droneSearched;
   }
 
+  /**
+   * Search and update data of a given drone that founded using a given id.
+   * 
+   * @param droneId Drone's id that will be searched.
+   * @param drone Object with changes that will be persisted in the founded drone.
+   * @return Drone updated with given data.
+   */
   public Deliverydrone updateDrone(long droneId, Deliverydrone drone) {
     Deliverydrone droneSearched = this.getDroneById(droneId);
 
@@ -33,6 +52,11 @@ public class DeliverydroneService {
     return this.deliverydroneRepository.save(droneSearched);
   }
 
+  /**
+   * Delete a drone using a given drone's id.
+   * 
+   * @param droneId Drone's id that will be searched.
+   */
   public void deleteDrone(long droneId) {
     this.deliverydroneRepository.deleteById(droneId);;
   }
